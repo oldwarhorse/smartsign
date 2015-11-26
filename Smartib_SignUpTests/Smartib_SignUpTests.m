7a95e8be15fa194fc4926eb1a1218d306551ebff
oldwarhorse <jpsurese@gmail.com>
2015-11-26 15:53:05 +0530

    add tests

---
+++ Smartib_SignUpTests/Smartib_SignUpTests.m
@@ -0,0 +1,40 @@
+//
+//  Smartib_SignUpTests.m
+//  Smartib_SignUpTests
+//
+//  Created by Leela Electronics on 25/11/15.
+//  Copyright (c) 2015 Leela Electronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Smartib_SignUpTests : XCTestCase
+
+@end
+
+@implementation Smartib_SignUpTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
